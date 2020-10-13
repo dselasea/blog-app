e9605e257033f0ee9c646a5ddb089afb501eaaad
Daniel Selase Anyidoho <dselasea@gmail.com>
2020-10-13 22:01:03 +0000

    First Blog App Commit with Stateless Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Courses from "./components/Courses";
+import Partners from "./components/Partners";
+import Content from "./components/Content";
+import Learn from "./components/Learn";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light navbar-fixed-top bg-whites">
+          <a className="navbar-brand" href="#">
+            Blog App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  <i className="fas fa-home"></i>
+                  <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#courses">
+                  Courses
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Log In
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      {/*<!-- Content -->*/}
+      <Content />
+
+      <Learn />
+      {/*<!-- Courses -->*/}
+      <Courses />
+
+      {/*<!-- Partnerss -->*/}
+      <Partners />
     </div>
   );
 }
---
+++ src/components/Courses.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <div className="container py-5" id="courses">
+      <div className="row">
+        <div className="col-md-8 offset-md-2 text-center pb-5">
+          <h2>Our Courses</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, expedita laudantium. Suscipit commodi et non?
+          </p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card p-2" style={{ width: "20rem" }}>
+            <div className="card-body text-center">
+              <i className="fas fa-mobile-alt fa-3x py-2 courses-icon"></i>
+              <h5 className="card-title">Mobile App Development</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card p-2" style={{ width: "20rem" }}>
+            <div className="card-body text-center">
+              <i className="fas fa-laptop-code fa-3x py-2 courses-icon"></i>
+              <h5 className="card-title">Web Development</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card p-2" style={{ width: "20rem" }}>
+            <div className="card-body text-center">
+              <i className="fas fa-cogs fa-3x py-2 courses-icon"></i>
+              <h5 className="card-title">Machine Learning</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Courses;
